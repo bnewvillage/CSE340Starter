e90e6b1b6f350628dd9858614514c0fd0da3ba01
bnewvillage <blainenewvillage@gmail.com>
2025-11-15 08:42:05 +0400

    testing

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseConstroller = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
 * View Engine and Templates
@@ -24,9 +26,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 //Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/",baseConstroller.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
