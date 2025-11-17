a3a1171e590a95e39d952cb222c3e48c55926414
bnewvillage <blainenewvillage@gmail.com>
2025-11-17 04:09:11 +0400

    fixed views

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,6 @@ const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
 //Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classification_id", invController.buildByClassificationId);
 
 module.exports = router;
