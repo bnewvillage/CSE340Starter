1983d945e6184f62460d5c74fff015a6d6c3b2a0
bnewvillage <blainenewvillage@gmail.com>
2025-11-19 21:57:19 +0400

    fix again

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,7 +1,39 @@
+--WEEK 2 ACTIVITIES
+--1. CREATE new account Tony Stark
+
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+)
+VALUES (
+    'Tony',
+    'Stark',
+    'tony@starkent.com',
+    'Iam1ronM@n'
+);
+
+--2. UPDATE Tony account type to `Admin`
+UPDATE public.account
+SET account_type = 'Admin'::account_type
+WHERE account_id = 1;
+
+--3. DELETE TONY
+DELETE FROM public.account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
 --4. UPDATE GM Hummer
 UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors','a huge interior')
 WHERE inv_id = 10;
+
+--5. SELECT Sports vehicles by make and model
+SELECT inv_make, inv_model
+FROM public.inventory
+INNER JOIN public.classification
+	ON inventory.classification_id = classification.classification_id
+WHERE inventory.classification_id = 2;
 
 --6. Update all recrods /vehicles
 UPDATE public.inventory
